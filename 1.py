180a20d10d364f573eed7bfadace441df3a8a5ef
timuruzakbaevhse <utbpag06@gmail.com>
2021-10-02 00:55:05 +0300

    added 3 files

---
+++ 1.py
@@ -0,0 +1,9 @@
+Surname=input()
+Name=input()
+Date_of_birth=input()
+result=Surname+"_"+Name+"_"+Date_of_birth
+print(result)
+Date_of_birth=int(Date_of_birth)+60
+Date_of_birth=str(Date_of_birth)
+result2=Name+"_"+Surname+"_"+Date_of_birth
+print(result2)
---
+++ 2.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=(a**2+b**2)**0.5
+area=0.5*a*b
+print(area)
+perimeter=a+b+c
+print(perimeter)
